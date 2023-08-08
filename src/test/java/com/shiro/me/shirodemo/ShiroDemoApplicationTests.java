5896fb4585edff62757d42a4a1c9933dd678d67e
beiguiyan <739112855@qq.com>
2023-08-09 07:16:19 +0800

    Initial commit

---
+++ src/test/java/com/shiro/me/shirodemo/ShiroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiro.me.shirodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
